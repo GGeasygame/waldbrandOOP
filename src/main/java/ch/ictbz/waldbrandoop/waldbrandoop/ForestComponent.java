80ec32edc0ec0bc08474668af8b5953a8a9add9b
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2022-01-26 14:33:53 +0100

    VERSION 1.0

--- src/main/java/ch/ictbz/waldbrandoop/waldbrandoop/ForestComponent.java
+++ src/main/java/ch/ictbz/waldbrandoop/waldbrandoop/ForestComponent.java
@@ -3,7 +3,7 @@ package ch.ictbz.waldbrandoop.waldbrandoop;
 import javafx.scene.shape.Circle;
 
 public class ForestComponent {
-    private final int RADIUS = 10;
+    private final int RADIUS = 5;
     protected Circle component = new Circle();
 
     protected ForestComponent() {
