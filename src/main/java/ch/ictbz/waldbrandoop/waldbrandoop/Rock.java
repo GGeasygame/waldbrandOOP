0067b940235ed88166a3d6f652e673b9d2a2a299
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2022-01-27 16:42:32 +0100

    pictures

--- src/main/java/ch/ictbz/waldbrandoop/waldbrandoop/Rock.java
+++ src/main/java/ch/ictbz/waldbrandoop/waldbrandoop/Rock.java
@@ -3,7 +3,8 @@ package ch.ictbz.waldbrandoop.waldbrandoop;
 public class Rock extends ForestComponent{
 
     public Rock() {
-        component = "/Users/jonaskaufmann/Desktop/BLJ/OOP/waldbrandOOP/src/main/resources/ch/ictbz/waldbrandoop/waldbrandoop/stone.png";
+        // component = "/Users/jonaskaufmann/Desktop/BLJ/OOP/waldbrandOOP/src/main/resources/ch/ictbz/waldbrandoop/waldbrandoop/stone.png";
+        component = "./images/stone.png";
     }
 
 }
